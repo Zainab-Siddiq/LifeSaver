ef3af4763a9e057d508e4a3d6fd6671b51e7612a
Zainab-Siddiq <zennisiddiq05@gmail.com>
2025-06-03 16:35:04 +0100

    Update Responder.java

--- Responder.java
+++ Responder.java
@@ -1,20 +1,4 @@
-// public class Responder extends User {
 
-//     public Responder(String name, String phoneNumber) {
-//         super(name, phoneNumber);
-//     }
-
-//     public void respondToAlert(Alert alert) {
-//         System.out.println("Responder " + getName() + " is responding to the alert.");
-//         // Simulation logic here
-//     }
-
-//     @Override
-//     public void displayInfo() {
-//         System.out.println("Responder Name: " + getName());
-//         System.out.println("Phone Number: " + getPhoneNumber());
-//     }
-// }
 
 class Responder extends User {
     private String department;
