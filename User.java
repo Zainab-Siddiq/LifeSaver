729c57db3d2c33a5dde820c68f11fbf11b5c5819
Zainab-Siddiq <zennisiddiq05@gmail.com>
2025-06-03 16:34:27 +0100

    Update User.java

--- User.java
+++ User.java
@@ -1,34 +1,4 @@
 
-// public abstract class User {
-//     private String name;
-//     private String phoneNumber;
-
-//     public User(String name, String phoneNumber) {
-//         this.name = name;
-//         this.phoneNumber = phoneNumber;
-//     }
-
-//     // Getters
-//     public String getName() {
-//         return name;
-//     }
-
-//     public String getPhoneNumber() {
-//         return phoneNumber;
-//     }
-
-//     // Setters
-//     public void setName(String name) {
-//         this.name = name;
-//     }
-
-//     public void setPhoneNumber(String phoneNumber) {
-//         this.phoneNumber = phoneNumber;
-//     }
-
-//     // Abstract method to be implemented by subclasses
-//     public abstract void displayInfo();
-// }
 abstract class User {
     protected String name;
     protected String phone;
