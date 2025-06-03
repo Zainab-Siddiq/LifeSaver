77bc1d58140fd755a9fffe94805726dbf0e46a38
Zainab-Siddiq <zennisiddiq05@gmail.com>
2025-06-03 16:37:29 +0100

    Update Citizen.java

--- Citizen.java
+++ Citizen.java
@@ -1,46 +1,4 @@
 
-// import java.util.ArrayList;
-// import java.util.List;
-
-// public class Citizen extends User implements Locatable {
-//     private List<Contact> emergencyContacts;
-//     private Location location;
-
-//     public Citizen(String name, String phoneNumber) {
-//         super(name, phoneNumber);
-//         this.emergencyContacts = new ArrayList<>();
-//         this.location = new Location(0.0, 0.0); // Default location
-//     }
-
-//     public void addEmergencyContact(Contact contact) {
-//         emergencyContacts.add(contact);
-//     }
-
-//     public List<Contact> getEmergencyContacts() {
-//         return emergencyContacts;
-//     }
-
-//     public void reportAlert(Alert alert) {
-//         Alert.sendAlert(this); // Overloaded method for sending alert
-//     }
-
-//     @Override
-//     public Location getLocation() {
-//         return location;
-//     }
-
-//     public void updateLocation(double latitude, double longitude) {
-//         this.location.setLatitude(latitude);
-//         this.location.setLongitude(longitude);
-//     }
-
-//     @Override
-//     public void displayInfo() {
-//         System.out.println("Citizen Name: " + getName());
-//         System.out.println("Phone Number: " + getPhoneNumber());
-//     }
-// }
-
 import java.util.*;
 
 class Citizen extends User implements Locatable {
